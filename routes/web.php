f44fe9b47c9b1a0beee06d0c07ce2ff1c94c0ef6
yazdancode <yshabanei@gmail.com>
2025-09-15 14:45:24 +0330

    code ro dorost shod

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Core\Routing\Route;
 // ثبت route ها
 Route::get('/', 'HomeController@index');
 Route::get('/archive', 'ArchiveController@index');
+Route::get('/archive/articles', 'ArchiveController@articles');
+Route::get('/archive/products', 'ArchiveController@products');
 
 Route::add(['get','post', 'put'], '/a', static function (){
     echo 'welcome';
