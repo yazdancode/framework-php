554a440e209566ff10fbec8606c877966849135d
yazdancode <yshabanei@gmail.com>
2025-09-17 20:48:41 +0330

    router doorst shod

--- index.php
+++ index.php
@@ -1,25 +1,31 @@
 <?php
 
-use App\Core\Routing\Router;
 use Yazdan\Helpers\DebugHelper;
-
-
-
 
 include "bootstrap/init.php";
 
+// مسیر تعریف‌شده با پارامتر داینامیک
+$route = '/post/{slug}';
 
-// اجرای Router
-//$router = new Router();
-//
-//$router->run();
+// تبدیل مسیر به الگوی regex
+$route_pattern = '#^' . preg_replace('/\{(\w+)\}/', '(?<$1>[-%\w]+)', $route) . '$#';
 
-//$route = '/post/{slug}';
-$route_pattern = '#^/post/(?<slug>[-%\w]+)$#';
-
-$uri1 = 'post/what-is-php';
-
+// تست با URIهای مختلف
+$uri1 = '/post/what-is-php';
 $uri2 = '/post/why-you-must-chose-7learn';
 
-$result = preg_match($route_pattern, $uri1);
-DebugHelper::niceDump($result);
+// اجرای تست
+$result1 = preg_match($route_pattern, $uri1, $matches1);
+$result2 = preg_match($route_pattern, $uri2, $matches2);
+
+// خروجی‌ها
+DebugHelper::niceDump($route);
+DebugHelper::niceDump($route_pattern);
+
+DebugHelper::niceDump($uri1);
+DebugHelper::niceDump($result1);
+DebugHelper::niceDump($matches1);
+
+DebugHelper::niceDump($uri2);
+DebugHelper::niceDump($result2);
+DebugHelper::niceDump($matches2);
